f780cefe72fe3e735cd5612168bd6b5062895181
Jackson Hardy <jhardy92@gmail.com>
2018-12-22 01:11:02 -0500

    comments to apijs

--- src/util/api.js
+++ src/util/api.js
@@ -1,74 +1,62 @@
+// api info kept in separate secure repo
 import { ACCESS_KEY, API_SECRET } from './config'
+
+// pulls photos from unsplash api based on dynamic query params
 export const fetchPhotos = (keyword, limit) => {
+
+    // sets headers
     const headers = {
         Authorization: `Client-ID ${ACCESS_KEY}`,
         secret: API_SECRET,
     }
 
+    // sets method
     const init = {
         method: 'GET',
         headers: headers,
     }
 
+    // if no param is generated in-app, defaults to 5 to avoid failure
     let countLimit = limit || 5
 
+    // if specific keyword is specified in-app, it is included in query
+    // otherwise query runs with no keyword param
     const newRequest = keyword ? 
     `https://api.unsplash.com/photos/random?count=${countLimit}&query=${keyword}&client_secret=${API_SECRET}` :
     `https://api.unsplash.com/photos/random?count=${countLimit}&client_secret=${API_SECRET}`
 
+    // actual api request
     return fetch(newRequest, init).then(response => {
         return response.json()
     }).then(responseBody => {
-        console.log(responseBody)
         return responseBody
     }).catch(error => {
         console.log(`the error was: ${error.message}`)
     })
-
-
 }
 
-    const forceDownload = (blob, filename) => {
-        let a = document.createElement('a');
-        a.download = filename;
-        a.href = blob;
-        a.click();
-    }
+// 'forces' a downlaod across clients 
+const forceDownload = (blob, filename) => {
+    let a = document.createElement('a');
+    a.download = filename;
+    a.href = blob;
+    a.click();
+}
 
-    // Current blob size limit is around 500MB for browsers
-    export const downloadResource = (url, filename, location) => {
-        if (!filename) filename = url.split('\\').pop().split('/').pop();
-        fetch(url, {
-            headers: new Headers({
-                'Origin': location
-            }),
-            mode: 'cors'
-        })
-        .then(response => response.blob())
-        .then(blob => {
-            let blobUrl = window.URL.createObjectURL(blob);
-            forceDownload(blobUrl, filename);
-        })
-        .catch(e => console.error(e));
-    }
-
-
-// export const downloadPhoto = (downloadLink) => {
-//     const headers = {
-//         Authorization: `Client-ID ${ACCESS_KEY}`,
-//         secret: API_SECRET,
-//     }
-
-//     const init = {
-//         method: 'GET',
-//         headers: headers,
-//     }
-
-//     const request = `downloadLink?client_secret=${API_SECRET}`
-
-//     return fetch(request, init).then(response => {
-//         return response.json()
-//     }).then(responseBody => {
-//         return responseBody
-//     })
-// }
+// Current blob size limit is around 500MB for browsers
+export const downloadResource = (url, filename, location) => {
+    if (!filename) filename = url.split('\\').pop().split('/').pop();
+    fetch(url, {
+        headers: new Headers({
+            'Origin': location
+        }),
+        mode: 'cors'
+    })
+    // blob is a funny word
+    .then(response => response.blob())
+    .then(blob => {
+        let blobUrl = window.URL.createObjectURL(blob);
+        forceDownload(blobUrl, filename);
+    })
+    .catch(e => console.error(e));
+}

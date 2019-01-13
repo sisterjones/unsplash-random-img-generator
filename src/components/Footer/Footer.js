4d932a2d0b2134796690d459c943f4458fa62ec7
Jackson Hardy <jhardy92@gmail.com>
2019-01-13 14:56:18 -0500

    lazy commit, minor change

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import './Footer.css'
+import Logo from './../Logo/Logo'
 
 const Footer = (props) =>  {
 
@@ -17,8 +18,19 @@ const Footer = (props) =>  {
 
         return (
             <footer className={`footer footer--${props.theme}`}>
-                I am the footer
-                <a onClick={toTop}>Back to Top</a>
+                <div className={`footer__top`}>
+                    <span className={`footer__copyright`}>&copy; <a href='https://github.com/sisterjones' target='_blank' rel='noreferrer' className={`footer__copyright footer__copyright--link`}>sister_jones</a> 2019</span>
+                    <a className={`footer__back-to-top`} onClick={toTop}>Back to Top</a>
+                    <a className={`footer__reset-grid`}>Reset Grid</a>
+                </div>
+                <div className={`footer__bottom`}>
+                    <div className={`footer__logo`}>
+                        <Logo 
+                            theme={props.theme}
+                        />
+                    </div>
+                    <p className={`footer__attribution`}>All images are available here thanks to the wonderful people at <a href="https://unsplash.com" target='_blank' rel='noreferrer' className={`footer__attribution footer__attribution--link`}>Unsplash</a> and their fantastic API. If you want curated sets of images, are looking to search by keyword, tag, author, etc., or want to support the talented photographers behind every one of these photos; head over to <a href="https://unsplash.com" target='_blank' rel='noreferrer' className={`footer__attribution footer__attribution--link`}>Unsplash.com</a>. Also note that I have no affiliation with Unsplash, their API, or any of their images.</p>
+                </div>
             </footer>
         )
 }

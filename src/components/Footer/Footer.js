78e6c5e646e4cf512adb2aa29c77f6deb524d4c1
Jackson Hardy <jhardy92@gmail.com>
2019-01-13 15:33:00 -0500

    everything working and looking like it should

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -19,17 +19,18 @@ const Footer = (props) =>  {
         return (
             <footer className={`footer footer--${props.theme}`}>
                 <div className={`footer__top`}>
-                    <span className={`footer__copyright`}>&copy; <a href='https://github.com/sisterjones' target='_blank' rel='noreferrer' className={`footer__copyright footer__copyright--link`}>sister_jones</a> 2019</span>
-                    <a className={`footer__back-to-top`} onClick={toTop}>Back to Top</a>
-                    <a className={`footer__reset-grid`}>Reset Grid</a>
+                    <a className={`footer__back-to-top footer__back-to-top--${props.theme}`} onClick={toTop}>Back to Top</a>
+                    {/* <a className={`footer__reset-grid footer__reset-grid--${props.theme}`}>Reset Grid</a> */}
                 </div>
+                <hr className={`footer__rule footer__rule--${props.theme}`}/>
                 <div className={`footer__bottom`}>
+                <span className={`footer__copyright footer__copyright--${props.theme}`}>&copy; <a href='https://github.com/sisterjones' target='_blank' rel='noreferrer' className={`footer__copyright footer__copyright--link footer__copyright--link--${props.theme}`}>sisterjones</a> 2019</span>
                     <div className={`footer__logo`}>
                         <Logo 
                             theme={props.theme}
                         />
                     </div>
-                    <p className={`footer__attribution`}>All images are available here thanks to the wonderful people at <a href="https://unsplash.com" target='_blank' rel='noreferrer' className={`footer__attribution footer__attribution--link`}>Unsplash</a> and their fantastic API. If you want curated sets of images, are looking to search by keyword, tag, author, etc., or want to support the talented photographers behind every one of these photos; head over to <a href="https://unsplash.com" target='_blank' rel='noreferrer' className={`footer__attribution footer__attribution--link`}>Unsplash.com</a>. Also note that I have no affiliation with Unsplash, their API, or any of their images.</p>
+                    <p className={`footer__attribution footer__attribution--${props.theme}`}>All images are available here thanks to the wonderful people at <a href="https://unsplash.com" target='_blank' rel='noreferrer' className={`footer__attribution footer__attribution--link footer__attribution--link--${props.theme}`}>Unsplash</a> and their fantastic API. If you want curated sets of images, are looking to search by keyword, tag, author, etc., or want to support the talented photographers behind every one of these photos; head over to <a href="https://unsplash.com" target='_blank' rel='noreferrer' className={`footer__attribution footer__attribution--link footer__attribution--link--${props.theme}`}>Unsplash.com</a>. Also note that I have no affiliation with Unsplash, their API, or any of their images.</p>
                 </div>
             </footer>
         )
